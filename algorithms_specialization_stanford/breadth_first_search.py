2d7bbff329c2a43eb6b360dae1e61633ac255fe1
grzegorz szopinski <szopinski.grzegorz@gmail.com>
2020-12-03 01:16:40 +0100

    Add draft implementation of breadth-first search algorithm

--- algorithms_specialization_stanford/breadth_first_search.py
+++ algorithms_specialization_stanford/breadth_first_search.py
@@ -1,2 +1,30 @@
+from queue import Queue
 from graph_utils import Graph
+
+# Example 8.2.3 from Algorithms Illuminated Part 2 - undirected graph with six vertices
+example_graph = Graph(['s', 'a', 'b', 'c', 'd', 'e'])
+
+example_graph.add_edge('s', 'a')
+example_graph.add_edge('s', 'b')
+example_graph.add_edge('a', 'c')
+example_graph.add_edge('b', 'c')
+example_graph.add_edge('c', 'e')
+example_graph.add_edge('c', 'd')
+example_graph.add_edge('d', 'e')
+
 # todo: BFS implementation
+# Initialize q with s
+explored = [example_graph.vertices[0]]
+q = Queue()
+q.put(example_graph.vertices[0])
+
+while not q.empty():
+    v = q.get()
+    print(f"current vertex: {v}")
+    for endpoint in example_graph.adj_list[v]:
+        print(f"current edge: {v, endpoint}")
+        if endpoint not in explored:
+            explored.append(endpoint)
+            q.put(endpoint)
+
+
